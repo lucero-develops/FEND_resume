7c73283889e767d5a43b4c440453fc2a07ab9c97
lucero-develops <mike.j.lucero@gmail.com>
2017-01-19 19:01:16 -0500

    created 2 vars and used .replace() to append "%data" with correct values

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,20 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Michael J Lucero");
+ //$("#main").append("Michael J Lucero");
+
+ /*var awesomeThoughts =
+ 	"My name is Michael and I'm Awesome!";
+
+ var funThoughts =
+ 	awesomeThoughts.replace("AWESOME!", "FUN");
+ $("#main").append(funThoughts);
+ */
+
+var formattedName = HTMLheaderName.replace("%data%", "Michael Lucero");
+$("#header").append(formattedName);
+
+var role = "Web Developer"
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(formattedRole);
+
