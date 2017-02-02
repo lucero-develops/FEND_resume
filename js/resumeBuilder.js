ba2c3644503484e46ae709c845aee0c641e03d2f
Michael Lucero <mlucero@abcorp.net>
2017-02-02 17:40:38 -0500

    Add my first object to helper js , then replaced %data% and appended my information

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,10 +12,43 @@ This is empty on purpose! Your code to build the resume will go here.
  $("#main").append(funThoughts);
  */
 
-var formattedName = HTMLheaderName.replace("%data%", "Michael Lucero");
-$("#header").append(formattedName);
+var bio ={
+	"name": "Michael Lucero",
+	"role": "Front-end Developer",
+	"contacts": {
+		"mobile": "219-629-9352",
+		"email": "lucero.develops@gmail.com",
+		"github": "lucero-develops",
+		"location": "Indianapolis"
+	},
+	"welcomeMessage": "Life begins where your comfortzone ends!",
+	"skills": [
+		"Customer Service", "SQL", "HTML", "CSS", "Java Script"],
+	"biopic": "images/tokyo.jpg"
+}
 
-var role = "Web Developer"
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-$("#header").append(formattedRole);
+
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	$("#header").append(formattedName);
+
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	$("#header").append(formattedRole);
+
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	$("#header, #footer-entry" ).append(formattedMobile);
+
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	$("#header, #footer-entry").append(formattedEmail);
+
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	$("#header, #footer-entry").append(formattedGithub);
+
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	$("#header, #footer-entry").append(formattedLocation);
+
+	var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+	$("#header").append(formattedWelcomeMsg);
+
+	var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+	$("#header").append(formattedPic);
 
